d2bf154fc4abfdbe0013d4f96a18577dcc75406f
naresh-1011 <samathamnaresh@gmail.com>
2024-11-28 16:14:40 +0530

    final_1

--- custom_nginx/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Replace placeholders in the template with actual environment variables
-envsubst < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
-
-# Start Nginx
-nginx -g "daemon off;"
-
